b277a480aab85544cb7d55dd6c4217a28f7c577e
Jr Reyes <jr@internetsoftwaresystems.com>
2015-07-22 13:02:04 +0800

    added pagination buttons

--- CourseHelper.php
+++ CourseHelper.php
@@ -13,18 +13,16 @@ Author URI:  https://github.com/aaronra
 /**
  * Proper way to enqueue scripts and styles
  */
-function emecanaan_scripts_basic()
-{
-}
+
 function emecanaan_scripts()
 {
     wp_register_script( 'emecanaan_script', plugins_url( 'js/emecanaan.js', __FILE__ ), array( 'jquery' ) );
     wp_enqueue_script('emecanaan_script');
 
     wp_enqueue_style('emecanaan', plugins_url('css/emecanaan.css', __FILE__));
-    emecanaan_scripts_basic();
+
 }
-function test_plugin_setup_menu(){
+function course_helper_setup_menu(){
     add_menu_page( 'Quiz Manager', 'Course Helper', 'manage_options', 'course-helper', 'add_cannaan_menu','dashicons-welcome-write-blog' );
 }
 
@@ -42,4 +40,4 @@ function add_cannaan_menu(){
 
 }
 add_action('wp_enqueue_scripts', 'emecanaan_scripts');
-add_action('admin_menu', 'test_plugin_setup_menu');
+add_action('admin_menu', 'course_helper_setup_menu');
--- js/emecanaan.js
+++ js/emecanaan.js
@@ -1,23 +1,74 @@
 jQuery(document).ready(function () {
+
+    var interval = setInterval(function () {
+        if (jQuery(".wpcw_fe_quiz_q_image").length) {
+            clearInterval(interval);
+            coursehelper();
+        }
+    }, 100);
+});
+
+function coursehelper() {
     var counter = 0;
+    console.log('courehelper js started');
+
     var obj = jQuery('.wpcw_fe_quiz_q_image').toArray();
     var images = jQuery.makeArray(obj);
     var image = jQuery(images[counter]);
     image.addClass('emecanaan-active');
-    jQuery('.wpcw_fe_quiz_box').append('<div class=><button class="emecanaan-back"> < </button><button class="emecanaan-next"> > </button></div>');
-    jQuery('.emecanaan-back').click(function() {
-        console.log('emecannaan');
-        if(counter === 0){
-            alert('Already last page');
+    var submitbutton = jQuery('.fe_btn.fe_btn_completion.btn_completion').toArray();;
+    submitbutton[0].value = 'Finish';
+    var answers = jQuery('.wpcw_fe_quiz_q_answers li input').toArray();
+    var paginator = '';
+    for (var answer in answers) {
+        answers[answer].checked = true;
+
+        if(counter == answer){
+            paginator = paginator+'<a class="page-number-active">'+answer+'</a>';
         }else{
+            paginator = paginator+'<a class="page-number">'+answer+'</a>';
+        }
+
+    }
+    jQuery('.wpcw_fe_quiz_q_image').append('<div class="paginator"><a class="emecanaan-back"> back </a><div id="paginatorNumbers">'+paginator+'</div><a class="emecanaan-next"> next </a></div>');
+    jQuery('.emecanaan-back').click(function () {
+        paginator = '';
+        for (var answer in answers) {
+            if(counter == answer){
+                paginator = paginator+'<a class="page-number-active">'+answer+'</a>';
+            }else{
+                paginator = paginator+'<a class="page-number">'+answer+'</a>';
+            }
+
+        }
+        jQuery('#paginatorNumber').html(paginator);
+        if (counter === 0) {
+            alert('Already last page');
+        } else {
+
+            var prevImage = jQuery(images[counter]);
+            prevImage.removeClass('emecanaan-active');
+
             counter--;
+            var nextImage = jQuery(images[counter]);
+            nextImage.addClass('emecanaan-active');
+
         }
     });
-    jQuery('.emecanaan-next').click(function() {
-        console.log('emecannaan');
-        if(counter === obj.length - 1){
+    jQuery('.emecanaan-next').click(function () {
+        paginator = '';
+        for (var answer in answers) {
+            if(counter == answer){
+                paginator = paginator+'<a class="page-number-active">'+answer+'</a>';
+            }else{
+                paginator = paginator+'<a class="page-number">'+answer+'</a>';
+            }
+
+        }
+        jQuery('#paginatorNumber').html(paginator);
+        if (counter === obj.length - 1) {
             alert('Already last page');
-        }else{
+        } else {
             var prevImage = jQuery(images[counter]);
             prevImage.removeClass('emecanaan-active');
 
@@ -27,4 +78,16 @@ jQuery(document).ready(function () {
         }
     });
 
-});
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
